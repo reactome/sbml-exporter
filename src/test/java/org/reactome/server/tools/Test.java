f5bc00f54b9143a2b5cd3b72c304b38a2616a080
Sarah Keating <skeating@caltech.edu>
2018-04-20 11:40:38 +0100

    remove my neo4j info

--- src/test/java/org/reactome/server/tools/Test.java
+++ src/test/java/org/reactome/server/tools/Test.java
@@ -46,7 +46,8 @@ public class Test {
                         new FlaggedOption("password", JSAP.STRING_PARSER, null, JSAP.REQUIRED, 'p', "password", "The neo4j password")
                 }
         );
-        String[] args = {"-h", "localhost", "-b", "7474", "-u", "neo4j", "-p", "j16a3s27"};
+        // need a String that contains the user information
+        String[] args = {"-h", "localhost", "-b", "7474", "-u", "neo4j", "-p", "INSERT PW HERE"};
         JSAPResult config = jsap.parse(args);
         if (jsap.messagePrinted()) System.exit(1);
         ReactomeGraphCore.initialise(config.getString("host"), config.getString("port"), config.getString("user"), config.getString("password"), GraphNeo4jConfig.class);
